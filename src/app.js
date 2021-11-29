13acbc391b38af5e95ed6d4da7609f8acff3af68
neverIand <64149697+neverIand@users.noreply.github.com>
2021-11-29 13:33:20 +0000

    added some error handling

--- src/app.js
+++ src/app.js
@@ -2,24 +2,97 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// TODO: add, subtract, multiply, divide
-// TODO: Error code
-app.get('/add/:num1/:num2', (req, res) => {
-  let result = 0
-  const num1 = Number(req.params.num1)
-  const num2 = Number(req.params.num2)
-  if (num1 == NaN || num2 == NaN) {
-    res.send('Invalid parameters')
-  }
-  console.log(`num1: ${num1}, num2: ${num2}`)
-  if (num1 && num2) {
-    result = num1 + num2
-    res.json(result)
-  } else {
-    res.send('Invalid parameters')
-  }
+// app.all('*', function (req, res, next) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   next()
+// })
+
+// TODO: Error handling e.g. /divide/4
+// TODO: implement subtract and multiply after everything works properly
+app.get('/add/:num1/:num2', function (req, res, next) {
+  let params = transformParams(req.params.num1, req.params.num2)
+  let result = add(params[0], params[1])
+  res.json(result)
 })
 
-// TODO: RESTful
+app.get('/divide/:num1/:num2', function (req, res, next) {
+  let params = transformParams(req.params.num1, req.params.num2)
+  let result = divide(params[0], params[1])
+  res.json(result)
+})
+
+// app.use('*', (err, req, res, next) => {
+//   console.error(err.stack)
+//   //res.status(404).send(err.message)
+//   let output = {
+//     error: 404,
+//     message: 'Not exist',
+//   }
+//   res.status(o9utput.error).json(output)
+// })
+
+// global error handling
+app.use((err, req, res, next) => {
+  //console.error(err.stack)
+  let output = {
+    error: err.httpStatusCode,
+    message: err.message
+  }
+  res.status(err.httpStatusCode).json(output)
+})
+
+
+
+/* 
+TODO: RESTful
+*/
+/*
+ *
+ *
+ */
+
+/* Check and transform parameters */
+function transformParams(param1, param2) {
+  console.log(`num1: ${param1}, num2: ${param2}`)
+  if (!param1 || !param2) {
+    let error = new Error('Missing Parameters')
+    error.httpStatusCode = 400
+    return next(error)
+  } else {
+    let num1 = Number(param1)
+    let num2 = Number(param2)
+    // console.log(`Converted num1: ${num1}, num2: ${num2}`)
+    if (isNaN(num1) || isNaN(num2)) {
+      let error = new Error('Invalid Parameters')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+
+    return [num1, num2]
+  }
+}
+
+/* Arithmetric functions */
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    let error = new Error('Zero Division')
+    error.httpStatusCode = 400
+    //return next(error)
+    throw error
+  }
+  return a / b
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
